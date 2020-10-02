6be9d51c2003cfc03a81e39ad5010ac83441b217
roboblox <supeecyborg@gmail.com>
2020-10-02 13:16:55 -0400

    added indexjs code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ Go code!
 
 const server = require("./server.js");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`\n***Server Running on http://localhost:${port} ***\n`);

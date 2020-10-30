d40d6e3c6e70b78db300c7c944a6678c6e62371c
ming <ming@localhost.localdomain>
2020-10-30 15:00:45 -0400

    changed router folders

--- api/server.js
+++ api/server.js
@@ -6,4 +6,4 @@ const server = express();
 
 // are where we configure the app/server
 server.use(express.json()); // gives Express the ability to parse the req.body
-server.use(postsRouter)
+server.use(projectsRouter)
